eb0e075605d5433e059c8368952530492dd93861
Yang Ma <1244139563@qq.com>
2019-06-25 14:43:49 +1000

    FIX: incoming changes

--- unreal_estate/server/settings/staging.py
+++ unreal_estate/server/settings/staging.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_2fry+twt1tv@uud_7wx1qh)jnfj@ftc=d^fefu#qw7*t1$*gx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
