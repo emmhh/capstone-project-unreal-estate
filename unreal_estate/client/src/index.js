623b2551df6deb37a3d5fdbc0f60a6a936ff1048
gaga0202 <gagannain0202@gmail.com>
2019-07-09 13:34:24 +1000

    FIX: nav bar issue

--- unreal_estate/client/src/index.js
+++ unreal_estate/client/src/index.js
@@ -3,10 +3,8 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './css/index.css';
 import App from './components/App';
-import Nav from './components/Nav';
 ReactDOM.render((
   <Router>
-    <Nav />
     <App />
   </Router>
 ), document.getElementById('root'));
