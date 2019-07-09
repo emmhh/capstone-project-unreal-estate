86015e9e3e5e0510970b830dc117bb79a9b857cf
gaga0202 <gagannain0202@gmail.com>
2019-07-09 16:57:20 +1000

    Feature: add user model

--- unreal_estate/user/models.py
+++ unreal_estate/user/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+  first_name = models.CharField(max_length=30)
+  last_name = models.CharField(max_length=30)
+  phone = models.IntegerField(max_length=9)
+  is_property_owner = models.BooleanField(default=False)
+  
