538ef98a29e843612282637cc87c297498794796
Y <yangma@Yangs-MacBook-Pro.local>
2019-07-16 15:19:50 +1000

    added the POST, GET, DELETE request on property

---
+++ unreal_estate/advertising/property.py
@@ -0,0 +1,92 @@
+from django.shortcuts import render
+from .models import Feature, Property, Rating, Reservation
+from django.http import Http404
+
+# Create your views here.
+def property_func (request):
+    if (request.method == "GET"):
+        propertyId = request.GET.get('propertyId')
+        property_ob = Property.objects.get(pk=propertyId)
+        response = {
+            # 'address': property.address,
+            #FIXME: make the attributes match up with property model.
+            'suburb': property_ob.suburb,
+            'city': property_ob.city,
+            'latitude': property_ob.latitude,
+            'longitude': property_ob.longitude,
+            'post_code': property_ob.post_code,
+            'num_room': property_ob.num_room,
+            'num_bathroom': property_ob.num_bathroom,
+            'num_guests': property_ob.num_guests,
+            'description': property_ob.description,
+            'space': property_ob.space,
+            'name': property_ob.name,
+            'building_type': property_ob.building_type,
+            'prices': property_ob.prices,
+            'avg_Rating': property_ob.avg_Rating,
+            'images': property_ob.imagesm,
+            # 'features': property_ob.features,  #this one should be a queryset,
+        }
+        return JSONResponse(response)
+    
+    elif (request.methods == "POST"):
+        propertyInfo = {
+            'suburb': request.POST.get('suburb'),
+            'city': request.POST.get('city'),
+            'latitude': request.POST.get('latitude'),
+            'longitude': request.POST.get('longitude'),
+            'post_code': request.POST.get('post_code'),
+            'num_room': request.POST.get('num_room'),
+            'num_bathroom': request.POST.get('num_bathroom'),
+            'num_guests': request.POST.get('num_guests'),
+            'description': request.POST.get('description'),
+            'space': request.POST.get('space'),
+            'name': request.POST.get('name'),
+            'building_type':request.POST.get('building_type'),
+            'prices': request.POST.get('prices'),
+            'avg_Rating': request.POST.get('avg_Rating'),
+            'images': request.POST.get('images'),
+        }
+        if (!propertyInfo['address'] or !propertyInfo['avg_Rating']
+            or !propertyInfo['num_Guests'] or !propertyInfo['description']
+            or !propertyInfo['name'] or !propertyInfo[building_type]
+            or !propertyInfo['prices'])
+            response = JsonResponse({'error': 'Required parameters not met.'})
+            response.status_code = 400
+            return response
+        
+        property_ob = Property()
+        property_ob.suburb = propertyInfo['suburb']
+        property_ob.city = propertyInfo['city']
+        property_ob.latitude = propertyInfo['latitude']
+        property_ob.longitude = propertyInfo['longitude']
+        property_ob.post_code = propertyInfo['post_code']
+        property_ob.num_room = propertyInfo['num_room']
+        property_ob.num_bathroom = propertyInfo['num_bathroom']
+        property_ob.num_guests = propertyInfo['num_guests']
+        property_ob.description = propertyInfo['description']
+        property_ob.space = propertyInfo['space']
+        property_ob.name = propertyInfo['name']
+        property_ob.building_type = propertyInfo['building_type']
+        property_ob.prices = propertyInfo['prices']
+        property_ob.avg_Rating = propertyInfo['avg_Rating']
+        property_ob.images = propertyInfo['images']
+
+        property_ob.save()
+
+        response = JsonResponse(property_ob)
+        return response
+
+    #to delete the property from the data base.
+    elif (request.methods == "DELETE") {
+        propertyId = request.GET.get('propertyId')
+        property_ob = Property.objects.get(pk=propertyId)
+        property_ob.delete()
+
+        response = JsonResponse({'success': 'successfully deleted property'})
+        return response
+    }
+
+
+def property_list (request, user_id):
+    pass
