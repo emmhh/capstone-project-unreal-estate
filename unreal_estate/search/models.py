365da7b221856d26105da39bf051cb0b44a243f0
Emmanuel Kozman <emmanuelkozman@hotmail.com>
2019-07-31 00:45:18 +1000

    Availability code implemented for search (havent tested)

--- unreal_estate/search/models.py
+++ unreal_estate/search/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from advertising.models import Property
 from django.contrib.gis.geos import Point
 from django.contrib.gis.measure import D
+from .models import Booking
 
 
 gmaps = googlemaps.Client(key='AIzaSyClDGqfGMbApqkFQ3SZbxG6dv7h7FDPCcA')
@@ -17,7 +18,38 @@ class PropertyManager(models.Model):
 		ref_location = Point(lng, lat, srid=4326)
 		properties = Property.objects.filter(location__distance_lte=(ref_location, D(km=maxDistance)), num_guests=numGuests).values()
 		properties = pointToNull(properties)
+
+		# code to remove any properties that have an overlapping booking from the properties results 
+		overlaps = []
+		for prop in properties:
+			bookings = Booking.objects.filter(property_id=prop['property_id']).values()
+			for booking in bookings:
+				# from https://stackoverflow.com/questions/325933/determine-whether-two-date-ranges-overlap
+				# note that we do not count two date ranges that end and start on the same day as an overlap and hence
+				# there is no <= and >= in the code below
+				if booking['startDate'] < endDate and booking['endDate'] > startDate:
+					overlaps.append(prop)
+					break
+		# removes any property thats inside the overlaps list
+		properties = [prop for prop in properties if prop not in overlaps]
+
 		return properties
+
+		'''
+		# AND NOW FOR A SINGLE PROPERTY - assuming prop is the property
+		overlap = False
+		bookings = Booking.objects.filter(property_id=prop['property_id']).values()
+		for booking in bookings:
+			if booking['startDate'] < endDate and booking['endDate'] > startDate:
+				overlap = True
+				break
+		# now whether there is an overlpa or not is stored in overlap
+		if overlap:
+			Foo()
+		else:
+			Bar()
+
+		'''
 
 
 	def searchByPropertyName(name):
