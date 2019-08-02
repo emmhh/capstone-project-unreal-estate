12c89c66cdb1fa04839c5712519c21686f42f883
gaga0202 <gagannain0202@gmail.com>
2019-08-03 08:56:35 +1000

    Feature: show route to destination on booking confirmation page

--- unreal_estate/client/src/components/google-maps/Route.js
+++ unreal_estate/client/src/components/google-maps/Route.js
@@ -9,28 +9,66 @@ class SimpleMap extends Component {
 
     this.state = {
       center: {
-        lat: 59.95,
-        lng: 30.33
+        lat: -33.8122763,
+        lng: 150.9668987
       },
       zoom: 11
     };
 
     this.apiIsLoaded = this.apiIsLoaded.bind(this);
+    this.getGeoLocation = this.getGeoLocation.bind(this);
+
   };
 
-  apiIsLoaded = (map, maps) => {
+  apiIsLoaded = async(map, maps) => {
     if (map) {
-      // do your thing with map or maps
-      console.log(map);
+      await this.getGeoLocation();
+      const directionsService = new maps.DirectionsService();
+      const directionsDisplay = new maps.DirectionsRenderer();
+      console.log(this.props.lat);
+      directionsDisplay.setMap(map);
+      directionsService.route({
+        origin: this.state.center,
+        destination: {
+            lat: this.props.lat,
+            lng: this.props.lng
+        },
+        travelMode: 'DRIVING'
+      }, (response, status) => {
+        if (status === 'OK') {
+          directionsDisplay.setDirections(response);
+        } else {
+          window.alert('Directions request failed due to ' + status);
+        }
+      });
     }
   };
+
+  getGeoLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          this.setState(prevState => ({
+            center: {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude,
+            },
+            zoom: prevState.zoom,
+          }));
+        }
+
+      )
+    } else {
+      console.log('There was an error getting current location.')
+    }
+  }
 
   render() {
     return (
       // Important! Always set the container height explicitly
       <div style={{ height: '100vh', width: '80%' }}>
         <GoogleMapReact
-          bootstrapURLKeys={{ key: 'AIzaSyCX7HaQnN4wU7IqNlQTDr1i5HAXt77LHQ8' }}
+          bootstrapURLKeys={{ key: 'AIzaSyClDGqfGMbApqkFQ3SZbxG6dv7h7FDPCcA' }}
           defaultCenter={this.state.center}
           center={this.state.center}
           defaultZoom={this.state.zoom}
@@ -38,8 +76,8 @@ class SimpleMap extends Component {
           yesIWantToUseGoogleMapApiInternals
           onGoogleApiLoaded={({ map, maps }) => this.apiIsLoaded(map, maps)}
         >
-          {/* {this.referencePointsList} */}
-          {/* {this.vehiclesInRoute} */}
+          {/* {this.referencePointsList}
+          {this.vehiclesInRoute} */}
         </GoogleMapReact>
       </div>
     );
--- unreal_estate/client/urls.py
+++ unreal_estate/client/urls.py
@@ -1,17 +1,9 @@
-from django.urls import path
+from django.conf.urls import url
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     path('', views.index ),
-    path('AdModule', views.index),
-    path('AdPreview', views.index),
-    path('AdForm/<property_id>', views.index),
-    path('signup', views.index),
-    path('login', views.index),
-    path('profile', views.index),
-    path('mybookings', views.index),
-    path('property/<property_id>', views.index),
-    path('property_booking/<property_id>', views.index),
-    path('confirmation/<booking_id>', views.index),
-    path('results', views.index),
+    url(r'^$', views.index),
+    url(r'^(?:.*)/?$', views.index),
 ]
