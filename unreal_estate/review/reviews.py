7388dc89004f3e6c8808a6302a3bb786b2b2dc39
Emmanuel Kozman <emmanuelkozman@hotmail.com>
2019-08-06 05:51:16 +1000

    update avg_rating for property when reviews added or updated

--- unreal_estate/review/reviews.py
+++ unreal_estate/review/reviews.py
@@ -19,86 +19,87 @@ logger = logging.getLogger(__name__)
 @csrf_exempt
 def SubmitReview(request):
 
-    # POST
-    if (request.method == "POST"):
-        debugLogger.debug("Create new review")
-        if not (request.user.is_authenticated):
-            debugLogger.info("the user is not logged in")
-            responce = JsonResponse({'error': "User is not logged in."})
-            responce.status_code = 403
-            return responce
+	# POST
+	if (request.method == "POST"):
+		debugLogger.debug("Create new review")
+		if not (request.user.is_authenticated):
+			debugLogger.info("the user is not logged in")
+			responce = JsonResponse({'error': "User is not logged in."})
+			responce.status_code = 403
+			return responce
 
 
-        # print("post: " + request.body.decode('utf-8'))
-        # print("user: " + str(request.user.id))
-        json_data = json.loads(request.body.decode('utf-8'))
+		# print("post: " + request.body.decode('utf-8'))
+		# print("user: " + str(request.user.id))
+		json_data = json.loads(request.body.decode('utf-8'))
 
-        propertyInstance = Property.objects.get(property_id=json_data['property_id'])
-        bookingInstance = Booking.objects.get(booking_id=json_data['booking_id'])
-        reviewDetails = {
-            'user_id':     request.user,
-            'property_id': propertyInstance,
-            'booking_id':  bookingInstance,
-            'value':       json_data['value'],
-            'notes':       json_data['notes'],
-        }
+		propertyInstance = Property.objects.get(property_id=json_data['property_id'])
+		bookingInstance = Booking.objects.get(booking_id=json_data['booking_id'])
+		reviewDetails = {
+			'user_id':	 request.user,
+			'property_id': propertyInstance,
+			'booking_id':  bookingInstance,
+			'value':	   json_data['value'],
+			'notes':	   json_data['notes'],
+		}
 
-    # Data Validation
-    if not (
-        reviewDetails['user_id'] and \
-        reviewDetails['property_id'] and \
-        reviewDetails['booking_id'] and \
-        reviewDetails['value'] and \
-        reviewDetails['notes']):
-        response = JsonResponse({'error': 'Required parameters not met.'})
-        response.status_code = 400
-        return response
-    review = Rating()
-    review.user_id = reviewDetails['user_id']
-    review.property_id = reviewDetails['property_id']
-    review.booking_id = reviewDetails['booking_id']
-    review.value = reviewDetails['value']
-    review.notes = reviewDetails['notes']
-    # Save new review 
-    try:
-        review.save()
-    except IntegrityError as ex:
-        if (ex.__cause__.pgcode == '23505'):
-            response = JsonResponse({'error': 'duplicate entry'})
-            response.status_code = 400
-            return response
-    response = JsonResponse({'review':review.rating_id}) # return booking id to test
-    return response
+	# Data Validation
+	if not (
+		reviewDetails['user_id'] and \
+		reviewDetails['property_id'] and \
+		reviewDetails['booking_id'] and \
+		reviewDetails['value'] and \
+		reviewDetails['notes']):
+		response = JsonResponse({'error': 'Required parameters not met.'})
+		response.status_code = 400
+		return response
+	review = Rating()
+	review.user_id = reviewDetails['user_id']
+	review.property_id = reviewDetails['property_id']
+	review.booking_id = reviewDetails['booking_id']
+	review.value = reviewDetails['value']
+	review.notes = reviewDetails['notes']
+	# Save new review 
+	try:
+		review.save()
+	except IntegrityError as ex:
+		if (ex.__cause__.pgcode == '23505'):
+			response = JsonResponse({'error': 'duplicate entry'})
+			response.status_code = 400
+			return response
+	UpdateAvgRatingForProperty(json_data['property_id'])
+	response = JsonResponse({'review':review.rating_id}) # return booking id to test
+	return response
 
 @csrf_exempt
 def ReviewDetailsBID(request, booking_id):
-    # GET
-    if (request.method == "GET"):
-        debugLogger.debug("Get booking details BID")
-        # Retrieve old booking
-        try:
-            review = Rating.objects.filter(booking_id=booking_id)[:1].get()
-        except ObjectDoesNotExist:
-            response = JsonResponse({'error': 'rating does not exist'})
-            response.status_code = 403
-            return response
-        reviewDetails = {
-            'rating_id'     : review.rating_id,
-            'property_id'   : review.property_id.property_id,
-            'user_id'       : review.user_id.id,
-            'booking_id'    : booking_id,
-            'value'         : review.value,
-            'date'          : review.date,
-            'notes'         : review.notes,
-        }
-        # print(reviewDetails['rating_id'])
-        # print(reviewDetails['property_id'])
-        # print(reviewDetails['user_id'])
-        # print(reviewDetails['booking_id'])
-        # print(reviewDetails['value'])
-        # print(reviewDetails['date'])
-        # print(reviewDetails['notes'])
-        return JsonResponse(reviewDetails)
+	# GET
+	if (request.method == "GET"):
+		debugLogger.debug("Get booking details BID")
+		# Retrieve old booking
+		try:
+			review = Rating.objects.filter(booking_id=booking_id)[:1].get()
+		except ObjectDoesNotExist:
+			response = JsonResponse({'error': 'rating does not exist'})
+			response.status_code = 403
+			return response
+		reviewDetails = {
+			'rating_id'	 : review.rating_id,
+			'property_id'   : review.property_id.property_id,
+			'user_id'	   : review.user_id.id,
+			'booking_id'	: booking_id,
+			'value'		 : review.value,
+			'date'		  : review.date,
+			'notes'		 : review.notes,
+		}
+		# print(reviewDetails['rating_id'])
+		# print(reviewDetails['property_id'])
+		# print(reviewDetails['user_id'])
+		# print(reviewDetails['booking_id'])
+		# print(reviewDetails['value'])
+		# print(reviewDetails['date'])
+		# print(reviewDetails['notes'])
+		return JsonResponse(reviewDetails)
 
 @csrf_exempt
 def GetRatingsByPropertyId (request, property_id):
@@ -134,5 +135,28 @@ def UpdateRatingsWithClassifications (request, property_id):
 	if len(texts) > 0:
 		for i in range(len(ratings)):
 			ratings[i]['ai_rating'] = classifications[i]
+	UpdateAvgRatingForProperty(property_id)
 
 	return JsonResponse({'results': list(ratings)})
+
+def UpdateAvgRatingForProperty(property_id):
+	print(property_id)
+	prop = Property.objects.get(property_id=property_id)
+	ratings = Rating.objects.filter(property_id=property_id)
+	if len(ratings) == 0:
+		return
+
+	n = len(ratings)
+	k = 0.10
+	numTrimmed = int(n*k)
+	values = []
+	for r in ratings:
+		values.append(float(r.value))
+	values = sorted(values)
+	values = values[numTrimmed:n-numTrimmed]
+	avg = sum(values)/len(values)
+
+	avg = round(avg,1)
+	
+	prop.avg_rating = avg
+	prop.save()
