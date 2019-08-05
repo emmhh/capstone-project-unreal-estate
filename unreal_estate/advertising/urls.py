c5c91f97c2dc1e67089cc49dfad4292e9cfe085f
clivche <clive41@gmail.com>
2019-08-06 00:10:07 +1000

    "commited again as previous"

--- unreal_estate/advertising/urls.py
+++ unreal_estate/advertising/urls.py
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-from django.urls import path
-from . import example
-from . import propertyClass
-#here redirect the url;
-#notes: put frontend files to client/src/components/...js
-#implement the calls as a seperate file called Advertising module;
-urlpatterns = [
-    path('example', example.exampleFunction),
-    path('<int:property_id>', propertyClass.PropertyFunction),
-    path('reviews/<int:property_id>', propertyClass.RatingFunction),
-    path('user', propertyClass.list_property),
-    path('new_property', propertyClass.addNew),
-    path('test_upload', propertyClass.testImageUpload)
-]
-=======
 from django.urls import path
 from . import example
 from . import propertyClass
@@ -27,4 +11,3 @@ urlpatterns = [
     path('new_property', propertyClass.addNew),
     path('test_upload', propertyClass.testImageUpload)
 ]
->>>>>>> b2e38a6142709814340c86b495e8cffe53331925
--- unreal_estate/review/models.py
+++ unreal_estate/review/models.py
@@ -1,19 +1,6 @@
 from django.db import models
 from django.core.exceptions import ObjectDoesNotExist
 
-<<<<<<< HEAD
-class Review(models.Model):
-    review_id = models.AutoField(primary_key=True)
-    booking_id = models.IntegerField()
-    review = models.FloatField()
-    
-    property_id = models.IntegerField()
-    startDate = models.DateField()
-    endDate = models.DateField()
-    bookingTime = models.DateTimeField(auto_now_add=True)
-    price = models.IntegerField()
-    num_guests = models.IntegerField(default=0)
-=======
 class Rating(models.Model):
     rating_id = models.AutoField(primary_key=True)
     property_id = models.ForeignKey('advertising.Property', on_delete=models.CASCADE, null=True)
@@ -25,4 +12,3 @@ class Rating(models.Model):
 
     def __str__ (self):
         return self.rating_id + self.property_id + self.value
->>>>>>> b2e38a6142709814340c86b495e8cffe53331925
