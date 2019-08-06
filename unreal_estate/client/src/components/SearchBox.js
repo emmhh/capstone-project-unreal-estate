b33d72f3749a40f243e8af4d779cad3f6b969870
Y <yangma@Yangs-MacBook-Pro.local>
2019-08-06 13:43:07 +1000

    Feature: display properties on home page

--- unreal_estate/client/src/components/SearchBox.js
+++ unreal_estate/client/src/components/SearchBox.js
@@ -5,6 +5,7 @@ import NumGuests from './NumGuests';
 import DatePickers from './DatePickers';
 import Button from '@material-ui/core/Button';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import PropertyRecoms from './PropertyRecoms';
 
 export default function SearchBox() {
 
@@ -17,6 +18,9 @@ export default function SearchBox() {
         <Link to='/results'>
           <Button variant="contained" style={{margin: "1%", verticalAlign: 'top'}}> Search </Button>
         </Link>
+      </div>
+      <div>
+        <PropertyRecoms/>
       </div>
     </div>
   );
